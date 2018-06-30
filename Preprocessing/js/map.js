ef0993d21d6044952197705e7fd33fc971f70898
Albert Mourato <ams11@cin.ufpe.br>
2018-06-30 19:25:38 -0300

    Adds highlight when mouse hovers on countries

--- Preprocessing/js/map.js
+++ Preprocessing/js/map.js
@@ -12,30 +12,15 @@ function clearMap(){
 
 // var colorScale = d3.scaleLinear().domain([0, 1366]).range(['#FFAFBA','#FF0400']);
 
+stateGroup = L.layerGroup().addTo(myMap);
 
-async function drawIncidentByState(){
-    stateGroup = L.layerGroup().addTo(myMap);
+var geojson;
+
+async function drawIncidentByState(){    
     d3.json('./data/countries.geojson', (error, data) => {
-        L.geoJSON(data, {
-            style (feature, layer) {
-                var x = groupBy('country', a);
-                var color;
-                if(x[(feature.properties.ADMIN).toUpperCase()] != undefined){
-                    colorScale(x[(feature.properties.ADMIN).toUpperCase()].length);
-                    // console.log(x[(feature.properties.ADMIN).toUpperCase()].length);
-                    color = colorScale(x[(feature.properties.ADMIN).toUpperCase()].length);
-                }else{
-                    color = "#A7A7A7";
-                }
-
-                return {
-                    fillColor: color,
-                    opacity: 1,
-                    weight: 1,
-                    color: "#FFF",
-                    fillOpacity: 1
-                }
-            }
+        geojson = L.geoJSON(data, {
+            style: style,
+            onEachFeature: onEachFeature
         }).addTo(stateGroup);
     });
 }
@@ -51,6 +36,50 @@ function colorScale(numberOfAttack){
     if(numberOfAttack < 500) return "#FF3D3A"
     if(numberOfAttack < 1000) return "#FF0400"
     if(numberOfAttack < 1500) return "#C80300"
-    return "#7C0200"
-    
+    return "#7C0200"   
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+    layer.setStyle({
+        weight: 3,
+        color: '#000',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function style(feature) {
+    var x = groupBy('country', a);
+    var color;
+    if(x[(feature.properties.ADMIN).toUpperCase()] != undefined){
+        colorScale(x[(feature.properties.ADMIN).toUpperCase()].length);
+        // console.log(x[(feature.properties.ADMIN).toUpperCase()].length);
+        color = colorScale(x[(feature.properties.ADMIN).toUpperCase()].length);
+    }else{
+        color = "#A7A7A7";
+    }
+
+    return {
+        fillColor: color,
+        opacity: 1,
+        weight: 1,
+        color: "#FFF",
+        fillOpacity: 1
+    }
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+    });
 }
