52310763b0462b818b1d247937766bc8dca2e96a
pglj2 <pglj2@cin.ufpe.br>
2018-07-02 19:25:38 -0300

    Added Donuts Charts

--- Preprocessing/js/index.js
+++ Preprocessing/js/index.js
@@ -1,5 +1,5 @@
 var histograms = [];
-
+var dornutCharts = [];
 var histWidth = 230;
 var histHeight = 420;
 
@@ -66,6 +66,11 @@ d3.csv("./data/attacks.csv", (data => {
     histograms.push(fatalHist);
     histograms.push(sexHist);
     histograms.push(typeHist);
+    
+    dornutChart(dictToList2('fatal') , '#fatalDornut' );
+    dornutChart(dictToList2('sex') , '#sexDornut');
+    dornutChart(dictToList2('type'), '#typeDornut');
+
 }));
  
 var a = [{}];
@@ -119,6 +124,19 @@ var dictToList = function(column){
     return list;
 }
 
+var dictToList2 = function(column){
+    var dict = groupBy(column, a);
+    var list = [];
+    for(var key in dict){
+        var temp = { type: key, value: dict[key].length  }; 
+        if(key == "") temp = { type: "Unknown", value: dict[key].length  }; 
+        
+        if(key != "undefined") list.push(temp);
+    }
+
+    return list;
+}
+
 var dictToListByCountry = function(column, country){
     var totalDict = groupBy(column, a);
     var dict = groupBy(column, groupBy('country', a)[country.toUpperCase()]);
@@ -130,3 +148,84 @@ var dictToListByCountry = function(column, country){
     }
     return list;
 }
+
+
+var dornutChart =  function(dataset, dv) {
+    'use strict';
+    var tooltip = d3.select(dv)            
+    .append('div')                             
+    .attr('class', 'tooltip');                 
+
+    tooltip.append('div')                        
+    .attr('class', 'label');                   
+
+    tooltip.append('div')                        
+    .attr('class', 'count');                   
+
+    tooltip.append('div')                        
+    .attr('class', 'percent');                 
+
+    var width = 360;
+    var height = 360;
+    var radius = Math.min(width, height) / 2;
+
+    var color = d3.scaleOrdinal(["#660000","#990000","#CC0000","#FF0000","#CC3333",
+    "#FF6666","#FF9999","#FFCCCC"]);
+
+    var svg = d3.select(dv)
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) + 
+        ',' + (height / 2) + ')');
+    
+    var donutWidth = 75;
+
+    var arc = d3.arc()
+    .innerRadius(radius - donutWidth)
+    .outerRadius(radius);
+
+    var pie = d3.pie()
+    .value(function(d) { return d.value; })
+    .sort(null);
+
+    var legendRectSize = 18;
+            var legendSpacing = 4;
+    
+    var path = svg.selectAll('path')
+    .data(pie(dataset))
+    .enter()
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', function(d, i) { 
+        return color(d.data.type);
+    
+    });
+    
+    var legend = svg.selectAll('.legend')
+    .data(color.domain())
+    .enter()
+    .append('g')
+    .attr('class', 'legend')
+    .attr('transform', function(d, i) {
+    var height = legendRectSize + legendSpacing;
+    var offset =  height * color.domain().length / 2;
+    var horz = -2 * legendRectSize;
+    var vert = i * height - offset;
+    return 'translate(' + horz + ',' + vert + ')';
+    });
+        
+    legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+        
+    legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) { return d; });
+
+}
+  
